bc253f64ae5ebcd23c5fa18c683863602ac3a8cc
Bill <bill@theresnobox.net>
2012-09-28 13:15:56 -0400

    Added some product views

--- controllers/product.js
+++ controllers/product.js
@@ -3,12 +3,16 @@ var Product = require('../models/product.js')
 module.exports = function(app){
 	app.get('/product', function(req, res){
 		Product.find(function(err, products){
-			res.send(products);	
+			res.render('product/list', {products: products, title: 'Products Page'});
 		});
 	});
 
-	app.get('/product/new', function(req, res){
-		new Product({name: 'Test', price: 1000}).save()
-		res.send(true);
+	app.get('/product/:id', function(req, res){
+		Product.findOne({_id: req.params.id}, function(err, product){
+			res.render('product/show', {product: product, title: 'Product Page'});
+		});
+
 	});
 };
+
+
